64aaa5d901d662375a1484f3d89469b87d5eeed7
Eric Young <tyoung4@Erics-MacBook-Air-2.local>
2020-09-25 14:06:25 -0700

    merkle attempt 1

--- crypto.py
+++ crypto.py
@@ -1,4 +1,5 @@
-
+import math
+import random
 # Caesar Cipher
 # Arguments: string, integer
 # Returns: string
@@ -56,13 +57,32 @@ def decrypt_help(numValue, offset):
     return eString
 
 
-# Vigenere Cipher
+#Vigenere Cipher
 # Arguments: string, string
 # Returns: string
-def encrypt_vigenere(plaintext,keyword):
-    plaintext_nums = ""
-    for character in plaintext:
-        ord(character) + (ord)
+def encrypt_vigenere(plaintext, keyword):
+    keyword = vigenere_key(plaintext, keyword)
+    encryptList = []
+    begNum = 65;
+    for character in range(len(plaintext)):
+        numValue = (ord(plaintext[character]) + ord(keyword[character])) % 26
+        numValue += begNum;
+        encryptList.append(chr(numValue))
+    encryptedWord = "".join(encryptList)
+    return encryptedWord
+
+# Arguments: string, string
+# Returns: string
+def decrypt_vigenere(ciphertext, keyword):
+    keyword = vigenere_key(ciphertext, keyword)
+    decryptList = []
+    begNum = 65
+    for character in range(len(ciphertext)):
+        numValue = (ord(ciphertext[character]) - ord(keyword[character])) % 26
+        numValue += begNum
+        decryptList.append(chr(numValue))
+    decryptedWord = "".join(decryptList)
+    return decryptedWord
 
 def vigenere_key(plaintext, keyword):
     keylist = list(keyword)
@@ -78,36 +98,79 @@ def vigenere_key(plaintext, keyword):
     return newKeyWord
 
 
-# Arguments: string, string
-# Returns: string
-def decrypt_vigenere(ciphertext, keyword):
-    pass
-
 # Merkle-Hellman Knapsack Cryptosystem
 # Arguments: integer
 # Returns: tuple (W, Q, R) - W a length-n tuple of integers, Q and R both integers
 def generate_private_key(n=8):
-    pass
+    W=[]
+    total = 1
+    for bit in range(n):
+        num = random.randint(total+1,2*total)
+        total += num
+        W.append(num)
+    Q = random.randint(total, 2*total)
+    R = random.randint(2, Q-1)
+    while(math.gcd(R, Q) != 1):
+          R = random.randint(2, Q-1)
+    return (tuple(W), Q, R)
 
 # Arguments: tuple (W, Q, R) - W a length-n tuple of integers, Q and R both integers
-# Returns: tuple B - a length-n tuple of integers
+# Returns: B - a length-n tuple of integers
 def create_public_key(private_key):
-    pass
+    W, Q, R = private_key
+    B =[]
+    for element in W:
+        B.append((R * element) % Q)
+    return tuple(B)
 
-# Arguments: string, tuple (W, Q, R)
+# Arguments: string, tuple B
 # Returns: list of integers
 def encrypt_mhkc(plaintext, public_key):
     pass
 
-# Arguments: list of integers, tuple B - a length-n tuple of integers
+def bits_to_byte(bits):
+    bitsAsStrings = []
+    for element in bits:
+        bitsAsStrings.append(str(element))
+        bitString = "".join(bitsAsStrings)
+        byte = int(bitString,2)
+    return byte
+
+def byte_to_bits(byte):
+    bits = []
+    binary = bin(byte)[2:]
+    for element in binary:
+        bits.append(element)
+    if (len(bits) < 8)
+        remainder = 8 % len(bits)
+        for num in range(0, remainder):
+            bits.insert(0,0)
+    return bits
+
+# Arguments: list of integers, private key (W, Q, R) with W a tuple.
 # Returns: bytearray or str of plaintext
 def decrypt_mhkc(ciphertext, private_key):
     pass
 
 def main():
     # Testing code here
-    print(encrypt_vigenere("ATTACKATDAWN", "LEMON"))
+    plaintext = "ATTACKATDAWN"
+    keyword = "LEMON"
+    
+    string1 = encrypt_caesar(" ", 5)
+    print("Caesar Encryption: " + string1)
+    string2 = decrypt_caesar(string1, 5)
+    print("Caesar Decryption: " + string2)
+    
+    
+    string3 = encrypt_vigenere(plaintext, keyword)
+    print("Vigenere Encryption: " + string3)
+    string4 = decrypt_vigenere(string3, keyword)
+    print("Vigenere Decryption: " + string4)
 
+    print (create_public_key(generate_private_key()))
+
+    print (bits_to_byte([0, 0, 0, 1, 0, 1, 0, 1]))
 if __name__ == "__main__":
     main()
 
